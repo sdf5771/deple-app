d82ef7da956e49881a3a8c756497d19ccd346190
seobisback <66990091+sdf5771@users.noreply.github.com>
2022-10-19 13:01:17 +0900

    [Front-end] - 20221019PM1301
    init project

---
+++ deple-app-front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+    return (
+        <Routes>
+            <Route path='/' element={} />
+            <Route path='/login' element={} />
+        </Routes>
+    );
+}
+
+export default App;
---
+++ deple-app-front-end/src/Route/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function Login() {
+    return (
+        <div><h1>hello i'm login</h1></div>
+    );
+}
+
+export default Login;
