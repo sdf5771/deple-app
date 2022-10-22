a03b25a694ad398b74e48cb355a909cad4c4cf3b
DESKTOP-L0CGF2I\kzsc5 <kzsc5464@naver.com>
2022-10-22 13:44:46 +0900

    Fix - login_API

--- deple-app-back-end/app/DB/DB_crud.py
+++ deple-app-back-end/app/DB/DB_crud.py
@@ -1,38 +1,52 @@
-from logging import exception
 from dotenv import load_dotenv
 import pymysql
 import json
 
 class DB_api:
     def __init__(self):
-        conn = pymysql.connect(
-        user='root', 
-        passwd='root', 
-        host='127.0.0.1', 
-        db='use', 
-        charset='utf8mb4_bin'
-        )
-
-def conn():
-    return pymysql.connect(
+        self.conn = None
+    
+    def connect(self):
+        self.conn = pymysql.connect(
         user='root', 
         passwd='root', 
         host='127.0.0.1', 
         db='user', 
-        charset='utf8mb4_bin'
+        charset='utf8'
         )
 
-def select(sql):
-    conn = conn()
-    with conn:
-        with conn.cursor() as cur:
-            try:
-                cur.execute(sql)
-                return json.dumps(cur.fetchall())
-            except exception as e:
-                print(e)
-            finally:
-                conn.close()
+    def login(self, sql):
+        self.connect()
+        rows = self.conn.cursor()
+        rows.execute(sql)
+        try:
+            return rows.fetchall()
+        except Exception as e:
+            print(e)
+        finally:
+            self.conn.close()
+
+
+# def conn():
+#     return pymysql.connect(
+#         user='root', 
+#         passwd='root', 
+#         host='127.0.0.1', 
+#         db='user', 
+#         charset='utf8mb4_bin'
+#         )
+
+# def select(sql):
+#     conn = conn()
+#     with conn:
+#         with conn.cursor() as cur:
+#             try:
+#                 cur.execute(sql)
+#                 return json.dumps(cur.fetchall())
+#             except exception as e:
+#                 print(e)
+#             finally:
+#                 conn.close()
 
 # def select(conn,sql):
 #     sql = "SELECT * FROM people"
