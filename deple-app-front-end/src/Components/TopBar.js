57cbfeda56594bd54f5753e42cfe8390e9b77cab
seobisback <66990091+sdf5771@users.noreply.github.com>
2022-10-23 21:57:26 +0900

    [Front-end]20221023PM2157

--- deple-app-front-end/src/Components/TopBar.js
+++ deple-app-front-end/src/Components/TopBar.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import propTypes from 'prop-types';
 import styles from '../Stylesheets/TopBar.module.css'
+import { useCookies } from 'react-cookie';
+import {useHref} from "react-router-dom";
+import PublicMessageBox from "./PublicMessageBox";
 
 function TopBarElement({}){
     const [elementVal, setElementVal] = React.useState();
@@ -14,6 +17,29 @@ function TopBarElement({}){
     );
 }
 
+function TopBarLogOutElement(){
+    const [cookie, setCookie , removeCookie] = useCookies([]);
+    const onClick = (event) => {
+        removeCookie('userId')
+        removeCookie('auth')
+
+        PublicMessageBox('로그아웃 되었습니다.');
+
+        setTimeout(()=>{
+            window.location.href = '/login'
+        },1000)
+    }
+
+    return (
+        <div onClick={onClick} className={styles.top_Bar_logout_element}>
+            <div className={styles.top_Bar_logout_img}></div>
+            <div className={styles.top_Bar_logout_title_div}>
+                <span>LogOut</span>
+            </div>
+        </div>
+    );
+}
+
 function TopBar() {
     return (
         <div id="TopBarRoot" className={styles.top_bar_root}>
@@ -21,7 +47,7 @@ function TopBar() {
                 <span className={styles.title_span}>DEPLE</span>
             </div>
             <div className={styles.top_bar_right_container}>
-
+                <TopBarLogOutElement />
             </div>
         </div>
     );
